6b8f2d4424bf6c4dec33986ecbc7de23a56110e1
Jordan Drake <jordan@jdrake.co.uk>
2019-10-06 16:51:45 +0100

    Solution

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,69 +1,105 @@
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+  name: string;
+  sellIn: number;
+  quality: number;
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export const AGED_BRIE = "Aged Brie";
+export const SULFURAS = "Sulfuras, Hand of Ragnaros";
+export const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+export const CONJURED = "Conjured";
+const MAX_QUALITY = 50;
+
+function increaseQuality(item: Item, amount: number = 1) {
+  item.quality = Math.min(item.quality + amount, MAX_QUALITY);
+}
+
+function decreaseQuality(item: Item, amount: number = 1) {
+  item.quality = Math.max(item.quality - amount, 0);
+}
+
+function updateSulfurasQuality(item: Item) {
+  item.quality = 80;
+}
+
+function updateAgedBrieQuality(item: Item) {
+  if (item.sellIn <= 0) {
+    increaseQuality(item, 2);
+  } else {
+    increaseQuality(item);
+  }
+
+  item.sellIn = item.sellIn - 1;
+}
+
+function updateBackstagePassQuality(item: Item) {
+  if (item.sellIn <= 0) {
+    item.quality = 0;
+  } else if (item.sellIn <= 5) {
+    increaseQuality(item, 3);
+  } else if (item.sellIn <= 10) {
+    increaseQuality(item, 2);
+  } else {
+    increaseQuality(item);
+  }
+
+  item.sellIn = item.sellIn - 1;
+}
+
+function updateNormalItemQuality(item: Item) {
+  const conjuredFactor = item.name.indexOf(CONJURED) > -1 ? 2 : 1;
+
+  if (item.sellIn <= 0) {
+    decreaseQuality(item, 2 * conjuredFactor);
+  } else {
+    decreaseQuality(item, conjuredFactor);
+  }
+
+  item.sellIn = item.sellIn - 1;
 }
 
 export class GildedRose {
-    items: Array<Item>;
+  items: Item[];
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+  constructor(items: Item[]) {
+    this.items = items;
+  }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+  updateQuality() {
+    // An alternate approach when there are a larger number of items
+    // could be to have a map of item names to handler function, for example:
+    //
+    // const handlers = { SULFURAS: updateSulfurasQuality };
+    // if (handlers[item.name]) { handlers[item.name](item); } else { updateNormalItemQuality(item); }
+    //
+    // However I personally think a switch statement is fine for 4 scenarios
 
-        return this.items;
-    }
+    this.items.forEach(item => {
+      switch (item.name) {
+        case SULFURAS:
+          updateSulfurasQuality(item);
+          break;
+
+        case AGED_BRIE:
+          updateAgedBrieQuality(item);
+          break;
+
+        case BACKSTAGE_PASS:
+          updateBackstagePassQuality(item);
+          break;
+
+        default:
+          updateNormalItemQuality(item);
+          break;
+      }
+    });
+
+    return this.items;
+  }
 }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,117 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { expect } from "chai";
+import {
+  Item,
+  GildedRose,
+  CONJURED,
+  AGED_BRIE,
+  SULFURAS,
+  BACKSTAGE_PASS
+} from "../app/gilded-rose";
 
-describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+describe("Gilded Rose", () => {
+  describe("a normal item", () => {
+    it("quality should decrease by 1 after a day", () => {
+      const gildedRose = new GildedRose([new Item("foo", 5, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(9);
     });
 
+    it("sellIn should decrease by 1 after a day", () => {
+      const gildedRose = new GildedRose([new Item("foo", 5, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(4);
+    });
+
+    it("quality should never go below 0", () => {
+      const gildedRose = new GildedRose([new Item("foo", 5, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+
+    it("quality should decrease by 2 if sellIn is passed", () => {
+      const gildedRose = new GildedRose([new Item("foo", 0, 4)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(2);
+    });
+  });
+
+  describe("a Conjured item", () => {
+    it("quality should decrease by 2 after a day", () => {
+      const gildedRose = new GildedRose([new Item(`${CONJURED} foo`, 5, 6)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(4);
+    });
+
+    it("quality should decrease by 4 if sellIn is passed", () => {
+      const gildedRose = new GildedRose([new Item(`${CONJURED} foo`, 0, 6)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(2);
+    });
+  });
+
+  describe("Aged Brie", () => {
+    it("quality should increase by 1 after a day", () => {
+      const gildedRose = new GildedRose([new Item(AGED_BRIE, 5, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(11);
+    });
+
+    it("quality should increase by 2 to increase past sellIn", () => {
+      const gildedRose = new GildedRose([new Item(AGED_BRIE, 0, 5)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(7);
+    });
+
+    it("quality should never go above 50", () => {
+      const gildedRose = new GildedRose([new Item(AGED_BRIE, 5, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+  });
+
+  describe("Sulfuras, Hand of Ragnaros", () => {
+    it("never decreases in quality", () => {
+      const gildedRose = new GildedRose([new Item(SULFURAS, 5, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(80);
+    });
+
+    it("never decreases sellIn", () => {
+      const gildedRose = new GildedRose([new Item(SULFURAS, 5, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(5);
+    });
+  });
+
+  describe("Backstage passes", () => {
+    it("quality should increase by 1 after a day if more than 10 days", () => {
+      const gildedRose = new GildedRose([new Item(BACKSTAGE_PASS, 11, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(11);
+    });
+
+    it("quality should increase by 2 after a day if 10 days or less left", () => {
+      const gildedRose = new GildedRose([new Item(BACKSTAGE_PASS, 10, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(12);
+    });
+
+    it("quality should increase by 3 after a day if 5 days or less left", () => {
+      const gildedRose = new GildedRose([new Item(BACKSTAGE_PASS, 5, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(13);
+    });
+
+    it("quality should never go above 50", () => {
+      const gildedRose = new GildedRose([new Item(BACKSTAGE_PASS, 5, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+
+    it("quality should be set to 0 if 0 days left", () => {
+      const gildedRose = new GildedRose([new Item(BACKSTAGE_PASS, 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+  });
 });
